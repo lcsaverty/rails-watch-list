6a47a772c47c8c88a60987c8b21dd0a36e08375b
Lucas Averty <lu.averty@hotmail.fr>
2023-02-23 20:51:57 +0100

    Done : 3 - Routes, contrôleur et vues pour les listes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  resources :movies
+  root "list#index"
+
+  resources :lists, only: [:new, :create, :show, :index, :destroy,] do
+    resources :bookmarks, only: [:new, :create,]
+  end
+
+  resources :bookmarks, only: [:destroy]
 end
