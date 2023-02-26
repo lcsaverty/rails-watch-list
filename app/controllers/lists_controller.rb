21e7fd37a56886af213f08a7eb7313e93c2a1731
Lucas Averty <lu.averty@hotmail.fr>
2023-02-26 18:24:37 +0100

    TODO : Customised the app

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,13 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: %i[show]
+  before_action :set_list, only: [:show, :destroy]
 
   def index
     @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
+    @review = Review.new(list: @list)
   end
 
   def new
@@ -11,24 +16,25 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-
     if @list.save
-      redirect_to lists_path(@list), notice: "List was successfully created."
+      redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  def show; end
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
 
   private
-
-  def list_params
-    params.require(:list).permit(:name)
-  end
 
   def set_list
     @list = List.find(params[:id])
   end
 
+  def list_params
+    params.require(:list).permit(:name, :photo)
+  end
 end
